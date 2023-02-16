6838d84c1aea1b71bdbcdd98b414a063a523dab6
qhy040404 <qhy040404@163.com>
2023-02-16 23:18:52 +0800

    dist

--- src/database-helper.ts
+++ src/database-helper.ts
@@ -1,5 +1,4 @@
 const sqlite = require('sqlite-sync');
-import * as core from '@actions/core'
 
 export function db_init(path: string) {
     sqlite.connect(path)
@@ -12,13 +11,13 @@ export function db_release() {
 export function insert(name: string, label: string, type: number, iconIndex: number): number {
     let insStr = `INSERT INTO rules_table (_id, name, label, type, iconIndex, isRegexRule, regexName) VALUES (null, '${name}', '${label}', ${type}, ${iconIndex}, 0, null)`
     let id = sqlite.run(insStr)
-    core.debug(String(id))
     return id
 }
 
 export function exists(name: string): boolean {
     let selectStr = `SELECT * FROM rules_table WHERE name = '${name}'`
-    let rows:any[] = sqlite.run(selectStr)
-    core.debug(rows.toString())
+    let rows = sqlite.run(selectStr)
+    console.log(name)
+    console.log(rows)
     return rows.length == 0
 }
